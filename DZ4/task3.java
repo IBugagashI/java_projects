b1983971f05915f6f1c3d70e19963a7d7edb200f
IBugagashI <popykin.alek@yandex.ru>
2023-03-26 17:26:15 +0300

    дз4. решение третьей  задачи

--- DZ4/task3.java
+++ DZ4/task3.java
@@ -4,13 +4,43 @@
 
 package DZ4;
 
+import java.util.LinkedList;
 import java.util.Scanner;
+
 
 public class task3 {
     public static void main(String[] args) {
+        LinkedList<String> calculator  = new LinkedList<>();
         Scanner iScanner = new Scanner(System.in);
-        System.out.println("");
-        int x = iScanner.nextInt();
-    }
+        System.out.println("Начните ввод. Для отмены последнего ввода, введите Delete: ");
+        while(true){
+            String x = iScanner.next();
+            if(x.equals("Delete")){
+                calculator.removeLast();
+            }
+            else{
+                calculator.add(x);
+            }
+            System.out.println("Результат = " + Result(calculator));
+        }
     
+    }
+    public static int Result(LinkedList<String> list){
+        int Result = Integer.parseInt(list.get(0));
+        for(int i = 1; i < list.size(); i++){
+            if(list.get(i - 1).equals("+")){
+                Result += Integer.parseInt(list.get(i));
+            }
+            else if (list.get(i - 1).equals("-")){
+                Result -= Integer.parseInt(list.get(i));
+            }
+            else if (list.get(i - 1).equals("*")){
+                Result *= Integer.parseInt(list.get(i));
+            }
+            else if (list.get(i - 1).equals("/")){
+                Result /= Integer.parseInt(list.get(i));
+            }
+        }
+        return Result;
+    }
 }
